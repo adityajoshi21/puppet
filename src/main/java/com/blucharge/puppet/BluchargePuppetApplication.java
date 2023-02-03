53c9645c87ce235dcc7b94172f220b78d725deec
Rishav Saxena <rishav.saxena@blu-smart.com>
2023-02-03 15:02:23 +0530

    Project setup done

---
+++ src/main/java/com/blucharge/puppet/BluchargePuppetApplication.java
@@ -0,0 +1,13 @@
+package com.blucharge.puppet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluchargePuppetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluchargePuppetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blucharge/puppet/BluchargePuppetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blucharge.puppet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluchargePuppetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
