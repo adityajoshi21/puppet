eea9408fbc571db2f0f2d139f2345662822c00a2
Aditya Joshi <adityajoshi@BMLAP258.local>
2023-03-17 15:08:27 +0530

    Minor changes

--- src/main/java/com/blucharge/puppet/config/SocketHandler.java
+++ src/main/java/com/blucharge/puppet/config/SocketHandler.java
@@ -1,6 +1,5 @@
 package com.blucharge.puppet.config;
 
-import com.blucharge.puppet.dto.req.BootNotificationReq;
 import com.blucharge.puppet.service.BootNotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +17,14 @@ import java.util.concurrent.ExecutionException;
 
 @Component
 @Slf4j
-public class SocketHandler extends TextWebSocketHandler {
-	
+public class SocketHandler extends TextWebSocketHandler{
 	private static final Map <String , WebSocketSession> chargeBoxToSessionMapping = new HashMap<>();
 	public static final  Map <WebSocketSession, String> sessionToChargeBoxIdMapping = new HashMap<>();
-
 	public static final String CHARGEBOX_ID_KEY = "chargepoint0234";
 	@Autowired
 	private BootNotificationService bootNotificationService;
 	@PostConstruct
 	public WebSocketSession createWebSocketConnection() throws ExecutionException, InterruptedException, IOException {
-//		if(sessions.containsKey(chargeBoxId))
-//			return sessions.get(chargeBoxId);
 		StandardWebSocketClient client = new StandardWebSocketClient();
 		WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
 		headers.add("Sec-WebSocket-Protocol","ocpp1.6");
@@ -50,19 +45,24 @@ public class SocketHandler extends TextWebSocketHandler {
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) // chargeboxid
 			throws  IOException {
 				System.out.println("3rd");
+				System.out.println(message.toString());
 				String currChargeBoxId =sessionToChargeBoxIdMapping.get(session);
 				WebSocketSession currSession = chargeBoxToSessionMapping.get(currChargeBoxId);
-				log.info("Current session established :" + currSession);
+				log.info("Current session: " + currSession +" with ChargeBoxId: " + currChargeBoxId);
 				currSession.sendMessage(message);
 	}
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("Connection Established");
+		System.out.println("Connection Established at: "+ session.getId());
 		chargeBoxToSessionMapping.put(CHARGEBOX_ID_KEY,session);
 		sessionToChargeBoxIdMapping.put(session, CHARGEBOX_ID_KEY);
 		String currChargeBoxId =sessionToChargeBoxIdMapping.get(session);
 		System.out.println("2nd");
-		handleMessage(session, new TextMessage(bootNotificationService.sendBootNotificationMessage(currChargeBoxId)));
+		System.out.println(currChargeBoxId);
+		TextMessage msg = new TextMessage(bootNotificationService.sendBootNotificationMessage(currChargeBoxId));
+		System.out.println(msg);
+		handleMessage(session, msg);
 	}
+
 }
