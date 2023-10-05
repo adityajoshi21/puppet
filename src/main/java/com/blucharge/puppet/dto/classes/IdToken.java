c2d7997d047a0ae71fce9db4c6a36c941ccf496f
aditya_bluSmart <202012108@daiict.ac.in>
2023-10-05 23:25:46 +0530

    Minor changes

--- src/main/java/com/blucharge/puppet/dto/classes/IdToken.java
+++
@@ -1,6 +0,0 @@
-package com.blucharge.puppet.dto.classes;
-
-public class IdToken {
-    public String idTag;
-
-}
--- src/main/java/com/blucharge/puppet/dto/enums/ChargePointStatus.java
+++
@@ -1,5 +0,0 @@
-package com.blucharge.puppet.dto.enums;
-
-public enum ChargePointStatus {
-    AVAILABLE, PREPARING, CHARGING, SUSPENDEDEVSE, SUSPENDEDEV, FINISHING, RESERVED
-}
